406497e5be663ca087216c25f3db2636cebd6a6b
Oliver Galante <oliverandcarli@gmail.com>
2023-10-19 17:04:14 -0400

    Backend of Book Libray practice

---
+++ api_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BookSerializer
+from .models import Book
+
+# Create your views here.
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all().order_by('year')
+    serializer_class = BookSerializer
